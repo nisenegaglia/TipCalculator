8c7b7867376c563c5c363a6cc6c3971799462455
Nicolle Senegaglia <125315391+nisenegaglia@users.noreply.github.com>
2023-03-27 20:43:31 -0300

    ids for start js/starting Js

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+// Seleção de Elementos
+
+const bill = document.querySelector("#bill");
+const billtotal = document.querySelector("#bill-total");
+const custom = document.querySelector("#custom");
+const peoplenumber = document.querySelector("#people-number");
+const people = document.querySelector("#people");
+const reset = document.querySelector("#reset");
+
+// Funções
+
+// Eventos
