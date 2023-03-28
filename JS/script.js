3bee4e9829ed5b92f5eb1c8500f33395108f3396
Nicolle Senegaglia <125315391+nisenegaglia@users.noreply.github.com>
2023-03-28 15:09:40 -0300

    add some functions

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,32 @@
 // Seleção de Elementos
 
-const bill = document.querySelector("#bill")
-const billtotal = document.querySelector("#bill-total")
-const custom = document.querySelector("#custom")
-const peoplenumber = document.querySelector("#people-number")
-const people = document.querySelector("#people")
-const reset = document.querySelector("#reset")
+const bill = document.querySelector("#bill") //form
+const peoplenumber = document.querySelector("#people-number") //form
+const billtotal = document.querySelector("#bill-total") //input
+const custom = document.querySelector("#custom") //input
+const people = document.querySelector("#people") // input
+const reset = document.querySelector("#reset") // input
 
 // Funções
 let billvalue = 0
 let peoplenumbervalue = 0
 let customvalur = 0
-// Eventos
+
+billtotal.addEventListener('input', function(){
+    biil = parseFloat (this.value)
+    makeCalculations()
+})
+
+custom.addEventListener('input', function(){
+    custom = parseFloat (this.value)
+    makeCalculations()
+})
+
+people.addEventListener('input', function(){
+    people = parseInt (this.value)
+    makeCalculations()
+})
+
+function reset (){
+    location.reload('reset')
+}
