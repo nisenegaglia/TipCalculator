f1af87ed3f9f920ae4f08c5e232100caecc8d8ca
Nicolle Senegaglia <125315391+nisenegaglia@users.noreply.github.com>
2023-03-27 22:23:42 -0300

    +Js development

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,14 @@
 // Seleção de Elementos
 
-const bill = document.querySelector("#bill");
-const billtotal = document.querySelector("#bill-total");
-const custom = document.querySelector("#custom");
-const peoplenumber = document.querySelector("#people-number");
-const people = document.querySelector("#people");
-const reset = document.querySelector("#reset");
+const bill = document.querySelector("#bill")
+const billtotal = document.querySelector("#bill-total")
+const custom = document.querySelector("#custom")
+const peoplenumber = document.querySelector("#people-number")
+const people = document.querySelector("#people")
+const reset = document.querySelector("#reset")
 
 // Funções
-
+let billvalue = 0
+let peoplenumbervalue = 0
+let customvalur = 0
 // Eventos
